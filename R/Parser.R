aa7931caeae1ed289a910e5fbabfa5316281c8b0
johndharrison <johndharrison0@gmail.com>
2014-04-28 22:57:48 +0700

    Add htmlParser and xmlParser methods to Parser class. Add documentation.

--- R/Parser.R
+++ R/Parser.R
@@ -14,13 +14,15 @@
 #' @section Methods:
 #' \describe{
 #' \item{\code{new(...)}:}{ Create a new \code{Parser} object. ... is used to define the appropriate slots.}
-#' \item{\code{getErrors(url)}:}{ Retrieve the parse errors, if any, from the last parse.}
+#' \item{\code{getErrors()}:}{ Retrieve the parse errors, if any, from the last parse.}
 #' \item{\code{parse(html, baseUri)}:}{ Parse HTML into a \code{\link{Document}}. The parser will make a sensible, balanced document tree out of any HTML.
 #' \describe{
 #' \item{\code{html: }}{A character string. HTML to parse}
 #' \item{\code{baseUri: }}{A boolean. The URL where the HTML was retrieved from. Used to resolve relative URLs to absolute URLs, that occur before the HTML declares a <base href> tag. If NA is specified, absolute URL detection relies on the HTML including a <base href> tag.}
 #' }
 #' }
+#' \item{\code{htmlParser()}:}{ Create a new HTML parser. This parser treats input as HTML5, and enforces the creation of a normalised document, based on a knowledge of the semantics of the incoming tags. }
+#' \item{\code{xmlParser()}:}{ Create a new XML parser. This parser assumes no knowledge of the incoming tags and does not treat it as HTML, rather creates a simple tree directly from the input. }
 #' }
 #' @export Parser
 #' @exportClass Parser
@@ -42,6 +44,14 @@ Parser <- setRefClass("Parser",
                           
                           parse = function(html, baseUri){
                             Document$new(document = parser$parse(html = html, baseUri = baseUri))$import(Jsoup())
+                          },
+                          
+                          htmlParser = function(){
+                            parser$htmlParser()
+                          },
+                          
+                          xmlParser = function(){
+                            parser$xmlParser()
                           }
                         )
 )
